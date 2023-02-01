19f68b097de0130e63082fd26c853b8a972e7032
jorge <jorge_eduardo2894@hotmail.com>
2023-02-01 08:42:33 -0800

    Made it more efficient

--- chaos_game/sierpinski_gasket.py
+++ chaos_game/sierpinski_gasket.py
@@ -2,13 +2,13 @@ import numpy as np
 import turtle
 
 
-def dot_in(t, x, y):
+def dot_in(t, coords):
     """
     Place a dot in the given coordinate.
     """
     # Draw the dot
     t.penup()
-    t.goto(x, y)
+    t.goto(coords[0], coords[1])
     t.pendown()
     t.dot()
 
@@ -27,15 +27,15 @@ turtle.pen(pensize=1)
 # Set three points for the triangle's vertices
 height = 700
 side = np.sqrt(4 * (height ** 2) / 3)
-pt_a = (0, height / 2)  
-pt_b = (side / 2, -height / 2)  
-pt_c = (-side / 2, -height / 2) 
+vertices = {'A': (0, height / 2), 
+            'B': (side / 2, -height / 2), 
+            'C': (-side / 2, -height / 2)}
 
 # Draw the three vertices
-dot_in(t, pt_a[0], pt_a[1])
-dot_in(t, pt_b[0], pt_b[1])
-dot_in(t, pt_c[0], pt_c[1])
-dot_in(t, 0, 0)  # Go home
+dot_in(t, vertices['A'])
+dot_in(t, vertices['B'])
+dot_in(t, vertices['C'])
+dot_in(t, (0, 0))  # Go home
 
 # Start the game
 turtle.pen(pensize=0.0001)
@@ -43,20 +43,8 @@ for iteration in range(500000):
     # Print the iteration number
     print(f"Current iteration: {iteration}")
 
-    # Throw the die
-    die_roll = np.random.randint(1, 7)
+    # Move halfway towards a vertex chosen at random
+    rand_vertex = np.random.choice(['A', 'B', 'C'])
+    new_pt = np.add(t.pos(), vertices[rand_vertex]) / 2
+    dot_in(t, new_pt)
 
-    # Move halfway towards A when we get 1 or 2 
-    if die_roll in [1, 2]:
-        new_pt = np.add(t.pos(), np.subtract(pt_a, t.pos()) / 2)
-        dot_in(t, new_pt[0], new_pt[1])
-
-    # Move halfway towards B when we get 3 or 4
-    elif die_roll in [3, 4]:
-        new_pt = np.add(t.pos(), np.subtract(pt_b, t.pos()) / 2)
-        dot_in(t, new_pt[0], new_pt[1])
-
-    # Move halfway towards C when we get 5 or 6
-    elif die_roll in [5, 6]:
-        new_pt = np.add(t.pos(), np.subtract(pt_c, t.pos()) / 2)
-        dot_in(t, new_pt[0], new_pt[1])
